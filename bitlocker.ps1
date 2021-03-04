9a473c93280a81fe0f287acf9e92c2763dbfcf81
Blake Miller <bmiller@junxion.tech>
2021-03-04 11:09:02 -0800

    Update Module Import

--- bitlocker.ps1
+++ bitlocker.ps1
@@ -1,4 +1,33 @@
-﻿import-module .\HuduAPI\HuduAPI.psd1 -Force
+﻿function Load-Module ($m) {
+
+    # If module is imported say that and do nothing
+    if (Get-Module | Where-Object {$_.Name -eq $m}) {
+        write-host "Module $m is already imported."
+    }
+    else {
+
+        # If module is not imported, but available on disk then import
+        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
+            Import-Module $m -Verbose
+        }
+        else {
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
+                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
+                Import-Module $m -Verbose
+            }
+            else {
+
+                # If module is not imported, not available and not in online gallery then abort
+                write-host "Module $m not imported, not available and not in online gallery, exiting."
+                EXIT 1
+            }
+        }
+    }
+}
+
+Load-Module "HuduAPI"
 
 #####################################################################
 $TOKEN = "##########"
@@ -36,3 +65,4 @@ $AssetObject = @{
 }
 
 Set-HuduAsset @AssetObject
+
